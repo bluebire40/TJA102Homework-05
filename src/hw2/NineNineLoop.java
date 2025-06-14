466404a95b4b10be631e5ca07dfc42cf13799838
L.W.Lian <bluebire40@gmail.com>
2025-06-14 21:00:57 +0800

    first commit

--- src/hw2/NineNineLoop.java
+++ src/hw2/NineNineLoop.java
@@ -3,23 +3,23 @@ package hw2;
 public class NineNineLoop {
 
 	public static void main(String[] args) {
-//		for (int i = 1; i <= 9; i++) {
-//			for (int j = 1; j <= 9; j++) {
-//				System.out.print(i + "*" + j + "=" + i * j + "\t");
-//			}
-//			System.out.println();
-//		}
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				System.out.print(i + "*" + j + "=" + i * j + "\t");
+			}
+			System.out.println();
+		}
 
-//		int i = 1;
-//		while (i <= 9) {
-//			int j = 1;
-//			while (j <= 9) {
-//				System.out.print(i + "*" + j + "=" + i * j + "\t");
-//				j++;
-//			}			
-//			System.out.println();
-//			i++;
-//		}
+		int i = 1;
+		while (i <= 9) {
+			int j = 1;
+			while (j <= 9) {
+				System.out.print(i + "*" + j + "=" + i * j + "\t");
+				j++;
+			}			
+			System.out.println();
+			i++;
+		}
 
 		int i = 1;
 		do {
